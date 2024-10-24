9e0f4b6face79fe0c2e95647cd3d65cf6d0f9979
donghyeon <thornappl2@gmail.com>
2024-10-24 16:59:42 +0900

    refactor: pass Kakao JavaScript key to template in CardPageController

--- src/main/java/com/devcard/devcard/card/controller/CardPageController.java
+++ src/main/java/com/devcard/devcard/card/controller/CardPageController.java
@@ -2,6 +2,7 @@ package com.devcard.devcard.card.controller;
 
 import com.devcard.devcard.card.dto.CardResponseDto;
 import com.devcard.devcard.card.service.CardService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,9 @@ public class CardPageController {
 
     private final CardService cardService;
 
+    @Value("${kakao.javascript.key}")
+    private String kakaoJavascriptKey;
+
     public CardPageController(CardService cardService) {
         this.cardService = cardService;
     }
@@ -20,6 +24,7 @@ public class CardPageController {
     public String viewCard(@PathVariable Long id, Model model) {
         CardResponseDto card = cardService.getCard(id);
         model.addAttribute("card", card);
+        model.addAttribute("kakaoJavascriptKey", kakaoJavascriptKey);
         return "cardDetail";
     }
 }
