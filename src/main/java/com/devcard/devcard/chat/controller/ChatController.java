9bd5920d213eb8663fc134914ce141f07bd69b81
cussle <cussle@kakao.com>
2024-10-24 15:36:37 +0900

    docs: Add comments to chat-related controllers

--- src/main/java/com/devcard/devcard/chat/controller/ChatController.java
+++ src/main/java/com/devcard/devcard/chat/controller/ChatController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 실시간 채팅(메시지 전송) 기능 관리
+ * 메시지 전송  API 제공
+ */
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
@@ -19,7 +23,11 @@ public class ChatController {
         this.chatService = chatService;
     }
 
-    // 1. 메세지 전송
+    /**
+     * 클라이언트로부터 메시지를 받아 해당 채팅방으로 전송
+     * @param sendingMessageRequest 전송하려는 메시지 정보를 담은 요청 객체
+     * @return 전송된 메시지 정보와 상태 코드 201 반환
+     */
     @PostMapping("")
     public ResponseEntity<SendingMessageResponse> sendMessage(@RequestBody SendingMessageRequest sendingMessageRequest) {
         return ResponseEntity.status(201).body(chatService.sendMessage(sendingMessageRequest));
