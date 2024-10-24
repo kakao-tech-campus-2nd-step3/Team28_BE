1536897d5b2223eefa9ee22c5cffc6319a3169cf
donghyeon <thornappl2@gmail.com>
2024-10-25 05:12:49 +0900

    feat(controller): pass Kakao JavaScript key to cardDetail template

--- src/main/java/com/devcard/devcard/card/controller/page/CardPageController.java
+++ src/main/java/com/devcard/devcard/card/controller/page/CardPageController.java
@@ -1,8 +1,9 @@
-package com.devcard.devcard.card.controller;
+package com.devcard.devcard.card.controller.page;
 
 import com.devcard.devcard.card.dto.CardResponseDto;
 import com.devcard.devcard.card.service.CardService;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +14,8 @@ public class CardPageController {
 
     private final CardService cardService;
 
-    @Value("${kakao.javascript.key}")
-    private String kakaoJavascriptKey;
+    @Autowired
+    private Environment env;
 
     public CardPageController(CardService cardService) {
         this.cardService = cardService;
@@ -24,7 +25,7 @@ public class CardPageController {
     public String viewCard(@PathVariable Long id, Model model) {
         CardResponseDto card = cardService.getCard(id);
         model.addAttribute("card", card);
-        model.addAttribute("kakaoJavascriptKey", kakaoJavascriptKey);
+        String kakaoJavascriptKey = env.getProperty("kakao.javascript.key");
         return "cardDetail";
     }
 }
